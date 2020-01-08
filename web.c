19ea35117dcddb5e9ef4dae9ee0f142c8dfd4f7a
canbin zhang <846917853@qq.com>
2020-01-08 22:26:19 +0800

    add bad_request()/* 返回给客户端这是个错误请求，HTTP 状态码 400 BAD REQUEST.*/

--- web.c
+++ web.c
@@ -22,6 +22,7 @@ void unsupported(int);
 void read(int, FILE *);  
 void serve_file(int, const char *);
 void not_found(int);
+void bad_request(int);
 int init(u_short *port)  
 {  
     int httpd = 0;  
@@ -188,4 +189,20 @@ void not_found(int client)
     send(client, buf, strlen(buf), 0);
     sprintf(buf, "</BODY></HTML>\r\n");
     send(client, buf, strlen(buf), 0);
+}
+void bad_request(int client)
+{
+    char buf[1024];
+
+    /*回应客户端错误的 HTTP 请求 */
+    sprintf(buf, "HTTP/1.0 400 BAD REQUEST\r\n");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "Content-type: text/html\r\n");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "<P>Your browser sent a bad request, ");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "such as a POST without a Content-Length.\r\n");
+    send(client, buf, sizeof(buf), 0);
 }
