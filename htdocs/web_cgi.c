0bfd00385c3910daaaa97b283842c716c3dc822c
canbin zhang <846917853@qq.com>
2020-01-09 02:31:44 +0800

    完善cgi响应程序，能够根据用户提交的数据进行加减乘除计算

--- htdocs/web_cgi.c
+++ htdocs/web_cgi.c
@@ -1,23 +1,47 @@
  #include <stdio.h>
  #include <stdlib.h>
+#include<string.h>
          int main(void)
          {
              char *data;
              long m,n;
+	     char c[10];
+	     char a[2];
              printf("Content-type: text/html\n\n");
-             printf("<TITLE>Mult Result</TITLE>");
-	     printf("<HEAD><meta charset='UFT-8'></HEAD>");
+             printf("<TITLE> Result</TITLE>");
+	     printf("<HEAD><meta charset='UTF-8'></HEAD>");
            printf("<H3>计算结果：</H3>");
-  
           data = getenv("QUERY_STRING");
           if(data == NULL)
-              printf("<P>请输入数字！");
-          else if(sscanf(data,"m=%ld&n=%ld",&m,&n)!=2)
-             printf("<P>数据错误，请输入正确的数字格式！");
-          else
-              printf("<P>%ld and %ld result: %ld", m, n, m * n);
-          printf("<br><h>感谢使用本服务器！！！</h1>");
+              printf("<P>有数据为空，请输入数据！！！");
+	  else 
+	  {
+		 
+		  sscanf(data,"m=%ld&n=%ld",&m,&n);
+		  int i=0;
+		  int j=0;
+		  for(i=0;i<strlen(data);i++)
+		  {
+			  if(data[i]=='e'&& data[i+1]=='=')
+			  {
+			
+	               for(j=0;j<3;j++){
+			       c[j]=data[i+2+j];
+		       }
+		
+       }
+  }
+if(c[0]=='a'){
+	printf("%ld + %ld = %ld",m,n,m+n);
+}if(c[0]=='s'){
+	printf("%ld - %ld = %ld",m,n,m-n);
+}if(c[0]=='m'){
+	printf("%ld × %ld = %ld",m,n,m*n);
+}if(c[0]=='d'){
+	printf("%ld ÷ %ld = %ld",m,n,m/n);
+}
+	  printf("<br><h>感谢使用本服务器！！！</h1>");
  
           return 0;
     }
-
+}
